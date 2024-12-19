aad3f98ad2e6de36ac56d7d6ae4ce1d634160c32
Akshay <akshaymangutkar951@gmail.com>
2024-12-19 18:55:35 +0530

    Integrate Redux Persist for state persistence and update App.js with PersistGate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import appStore from "./components/appStore";
+import appStore, { persistor } from "./components/appStore";
 import appRouter from "./components/Container";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={appStore}>
-      <RouterProvider router={appRouter} />;
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <RouterProvider router={appRouter} />
+      </PersistGate>
     </Provider>
   );
 }
--- src/components/appStore.js
+++ src/components/appStore.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import formReducer from "../components/formSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, formReducer);
 const appStore = configureStore({
   reducer: {
-    input: formReducer,
+    input: persistedReducer,
   },
 });
-
+export const persistor = persistStore(appStore);
 export default appStore;
